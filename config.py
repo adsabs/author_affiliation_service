14124cc0f6beb2b54efa0eec2bdad3cb7d8a5410
Sergi Blanco-Cuaresma <marblestation@users.noreply.github.com>
2018-08-07 09:53:23 -0400

    Enabled JSON stdout logging and HTTP connection pool

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 
+LOG_STDOUT = True
 
 AUTHOR_AFFILIATION_SOLRQUERY_URL = "http://api.adsabs.harvard.edu/v1/search/bigquery"
 AUTHOR_AFFILIATION_SERVICE_ADSWS_API_TOKEN = 'this is a secret api token!'
