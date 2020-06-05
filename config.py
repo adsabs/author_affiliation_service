4dac225444ab15ec2bbcd3512ded719a6037c240
Roman Chyla <roman.chyla@gmail.com>
2020-06-05 12:15:04 -0400

    Bugfix for the client

--- config.py
+++ config.py
@@ -4,3 +4,7 @@ LOG_STDOUT = True
 AUTHOR_AFFILIATION_SOLRQUERY_URL = "http://api.adsabs.harvard.edu/v1/search/bigquery"
 
 AUTHOR_AFFILATION_SERVICE_MAX_RECORDS_SOLR = 1000
+
+# must be here for adsmutils to override it using env vars
+# but if left empty (resolving to False) it won't be used
+SERVICE_TOKEN = None
